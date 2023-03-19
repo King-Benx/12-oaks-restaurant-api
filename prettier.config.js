3c450502db788583babfcaedbcd78d43b93b5908
Benard Asiimwe Bataringaya <benard.asiimwe.bataringaya@gmail.com>
2023-03-19 20:49:00 +0300

    chore(initial structure): implements initial structure of our api

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true,
+  printWidth: 120,
+  tabWidth: 2,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  arrowParens: 'always',
+};
