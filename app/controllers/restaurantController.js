c0f230ce6b3eef83cdd2098a23176dad222c1f51
Benard Asiimwe Bataringaya <benard.asiimwe.bataringaya@gmail.com>
2023-03-20 09:43:14 +0300

    feat(integrate yelp): integrates yelp to be able to retrieve information

--- app/controllers/restaurantController.js
+++ app/controllers/restaurantController.js
@@ -1,11 +1,56 @@
-// import { YELP_API_KEY } from '../config/env';
+import yelp from 'yelp-fusion';
+import { YELP_API_KEY } from '../config/env';
+import Logger from '../config/winston';
 
 const Restaurants = {
   getRestaurants: (req, res) => {
-    res.send('This was called!');
+    const { location, term } = req.query;
+
+    let searchRequest = {};
+    if (!location && !term) {
+      searchRequest = {
+        location: 'london',
+        term: 'restaurant',
+      };
+    }
+    if (!location && term) {
+      searchRequest = {
+        term,
+        location,
+      };
+    }
+
+    const client = yelp.client(YELP_API_KEY);
+
+    client
+      .search(searchRequest)
+      .then((response) => res.send(JSON.stringify(response.jsonBody.businesses)))
+      .catch((e) => {
+        if (e.statusCode === 400) {
+          res.status(400).send('Failure retrieving information due to bad request');
+        }
+        if (e.statusCode === 500) {
+          res.status(500).send('Internal Server error please try again');
+        }
+
+        Logger.error(e.message);
+      });
   },
   getSingleRestaurant: (req, res) => {
-    res.send('This was called!');
+    const client = yelp.client(YELP_API_KEY);
+    client
+      .business(req.params)
+      .then((response) => res.send(JSON.stringify(response.jsonBody)))
+      .catch((e) => {
+        if (e.statusCode === 400) {
+          res.status(400).send('Failure retrieving information due to bad request');
+        }
+        if (e.statusCode === 500) {
+          res.status(500).send('Internal Server error please try again');
+        }
+
+        Logger.error(e.message);
+      });
   },
 };
 
